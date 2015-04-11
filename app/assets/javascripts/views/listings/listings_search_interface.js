bf8283e716810adc7de2956a0b24136b1d5ef5a2
Brandon Lilly <brandonjlilly@gmail.com>
2015-04-10 18:17:19 -0700

    search price range element

--- app/assets/javascripts/views/listings/listings_search_interface.js
+++ app/assets/javascripts/views/listings/listings_search_interface.js
@@ -9,6 +9,29 @@ LastAirBnb.Views.ListingsSearchInterface = Backbone.View.extend({
     var content = this.template({ listings: this.collection });
     this.$el.html(content);
 
+
+    var minPrice = 10, maxPrice = 1000;
+    var $priceRange = this.$("#price-range");
+    $priceRange.slider({
+      range: true,
+      min: minPrice,
+      max: maxPrice,
+      slide: function (event, ui) {
+        // debugger
+        $("#price-min").html(ui.values[0] + ' yuan');
+        if (ui.values[1] < maxPrice) {
+          $("#price-max").html(ui.values[1] + ' yuan');
+        } else {
+          $("#price-max").html( maxPrice + '+ yuan');
+        }
+      }
+    });
+
+    // $priceRange.slider('option', 'slide').call($priceRange);
+
+    // $("#price-min").html(minPrice + ' yuan');
+    // $("#price-max").html(maxPrice + '+ yuan');
+
     this.collection.forEach(function (listing) {
       var view = new LastAirBnb.Views.ListingListItem({ model: listing });
       this.$('.listings-items').append(view.render().$el);
