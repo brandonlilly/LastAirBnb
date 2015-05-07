1f152e6618c2e408805d184b681c89717986bdfd
Brandon Lilly <brandonjlilly@gmail.com>
2015-05-07 14:17:26 -0700

    no carousel for single imgs

--- app/assets/javascripts/plugins/carousel.js
+++ app/assets/javascripts/plugins/carousel.js
@@ -6,12 +6,14 @@ $.Carousel = function (el) {
   this.activeIdx = 0;
   this.$imgs = this.$el.find('.cover-img');
 
-  $left =  $('<div class="cycle-left"><span class="glyphicon glyphicon-menu-left" aria-hidden="true"></span></div>');
-  $right = $('<div class="cycle-right"><span class="glyphicon glyphicon-menu-right" aria-hidden="true"></span></div>');
-  this.$el.append($left).append($right);
+  if (this.$imgs.length > 1) {
+    $left =  $('<div class="cycle-left"><span class="glyphicon glyphicon-menu-left" aria-hidden="true"></span></div>');
+    $right = $('<div class="cycle-right"><span class="glyphicon glyphicon-menu-right" aria-hidden="true"></span></div>');
+    this.$el.append($left).append($right);
 
-  $left.on('click', this.cycle.bind(this, -1));
-  $right.on('click', this.cycle.bind(this, 1));
+    $left.on('click', this.cycle.bind(this, -1));
+    $right.on('click', this.cycle.bind(this, 1));
+  }
 };
 
 $.Carousel.prototype.cycle = function (dir, event) {
